729d79822e033a8d4be9b6c45847c5e90f10b8eb
Ksawery <ksawery@superherocheesecake.com>
2020-08-21 12:37:56 +0200

    set up commit

---
+++ script/index.js
@@ -0,0 +1,49 @@
+// "use strict"
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry(20, 20, 20);
+
+const material = new THREE.MeshPhongMaterial({
+    color: 0xdaa520,
+    specular: 0x750a0a,
+    emissive: 0xdaa520,
+    alphaTest: 0.2,
+    shininess: 20,
+});
+
+
+const frontSpot = new THREE.SpotLight(0x750a0a);
+frontSpot.position.set(1000, 1000, 1000);
+scene.add(frontSpot);
+
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+const pointLight = new THREE.PointLight(0x750a0a, 2);
+pointLight.position.set(0, 20, 30);
+scene.add(pointLight);
+
+const sphereSize = 2;
+const pointLightHelper = new THREE.PointLightHelper(pointLight, sphereSize);
+scene.add(pointLightHelper)
+
+const render = function () {
+    requestAnimationFrame(render);
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y -= 0.02;
+    camera.updateProjectionMatrix();
+
+    renderer.render(scene, camera);
+};
+
+
+
+camera.position.z = 55;
+
+render();
