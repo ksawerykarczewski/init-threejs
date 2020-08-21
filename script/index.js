673aad0aa6ac46ae931356b080a14cec7dd37edb
Ksawery <ksawery@superherocheesecake.com>
2020-08-21 13:06:56 +0200

    2nd cube

--- script/index.js
+++ script/index.js
@@ -7,15 +7,21 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry(20, 20, 20);
+const geometry2 = new THREE.BoxGeometry(20, 20, 20);
 
 const material = new THREE.MeshPhongMaterial({
     color: 0xdaa520,
     specular: 0x750a0a,
     emissive: 0xdaa520,
-    alphaTest: 0.2,
     shininess: 20,
 });
 
+const material2 = new THREE.MeshPhongMaterial({
+    color: 0xdaa520,
+    specular: 0x750a0a,
+    emissive: 0xdaa520,
+    shininess: 20,
+});
 
 const frontSpot = new THREE.SpotLight(0x750a0a);
 frontSpot.position.set(1000, 1000, 1000);
@@ -23,6 +29,10 @@ scene.add(frontSpot);
 
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
+
+const cube2 = new THREE.Mesh(geometry2, material2);
+cube.rotation.x = 10;
+scene.add(cube2);
 
 const pointLight = new THREE.PointLight(0x750a0a, 2);
 pointLight.position.set(0, 20, 30);
@@ -36,13 +46,14 @@ const render = function () {
     requestAnimationFrame(render);
 
     cube.rotation.x += 0.01;
-    cube.rotation.y -= 0.02;
+    cube.rotation.y -= 0.01;
+
+    cube2.rotation.x += 0.01;
+    cube2.rotation.y -= 0.01;
     camera.updateProjectionMatrix();
 
     renderer.render(scene, camera);
 };
-
-
 
 camera.position.z = 55;
 
